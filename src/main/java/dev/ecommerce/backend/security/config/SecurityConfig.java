97d940e85ccc2991eb8c21b1e9cccf5ac95469cf
DevNhaan <nhandeptrai137@gmail.com>
2022-12-01 18:39:17 +0700

    test cors

--- src/main/java/dev/ecommerce/backend/security/config/SecurityConfig.java
+++ src/main/java/dev/ecommerce/backend/security/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.cors().configurationSource(request -> {
 			var cors = new CorsConfiguration();
-		      cors.setAllowedOrigins(List.of("http://localhost:3000/","http://ecommerce-a-api-v1.up.railway.app"));
+		      cors.setAllowedOrigins(List.of("http://localhost:3000/*","http://ecommerce-a-api-v1.up.railway.app/*"));
 		      cors.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "OPTIONS"));
 		      cors.setAllowedHeaders(List.of("*"));
 		      return cors;
